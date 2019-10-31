36524dbc0e043477551deb1a54506e6bb567d3ff
arunabhamidipati <arunprasad_bh@yahoo.com>
2019-10-31 16:38:33 +0000

    Added Validations to BeerDto Closes #8

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -9,6 +9,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -19,20 +23,33 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @Null
     private UUID id;
 
+    @Null
+    private Integer version;
+
+    @Null
+    private OffsetDateTime createDate;
+
+    @Null
+    private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
     private String beerName;
 
+    @NotNull
     private BeerStyle beerStyle;
 
+    @Positive
+    @NotNull
     private Long upc;
 
+    @Positive
+    @NotNull
     private Integer quantityOnHand;
 
     private BigDecimal price;
 
-    private OffsetDateTime createDate;
-
-    private OffsetDateTime lastModifiedDate;
 
 }
