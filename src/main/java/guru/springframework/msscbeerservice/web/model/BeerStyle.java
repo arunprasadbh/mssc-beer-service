21382734ff1b205eda85bf9bcabd7a52c01239fe
arunabhamidipati <arunprasad_bh@yahoo.com>
2019-10-31 15:01:29 +0000

    Adding BeerController. Closes #3

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyle.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyle.java
@@ -6,5 +6,5 @@ package guru.springframework.msscbeerservice.web.model;
 
 
 public enum BeerStyle {
-    LAGER,  PILSNER, STOUT GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
 }
