72da41786a994fb90223d50e42a3325d8da579c1
arunabhamidipati <arunprasad_bh@yahoo.com>
2019-12-03 11:08:36 +0000

    Enhanced Beer data with inventory information. Closes #18

---
+++ src/test/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
@@ -0,0 +1,33 @@
+package guru.springframework.msscbeerservice.services.inventory;
+
+import guru.springframework.msscbeerservice.bootstrap.Bootstrap;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/*
+ * Created by arunabhamidipati on 03/12/2019
+ */
+
+@Disabled
+@SpringBootTest
+class BeerInventoryServiceRestTemplateImplTest {
+
+    @Autowired
+    BeerInventoryService beerInventoryService;
+
+    @Test
+    void setBeerInventoryServiceHost() {
+    }
+
+    @Test
+    void getOnHandInventory() {
+
+        Integer qoh = beerInventoryService.getOnHandInventory(Bootstrap.BEER_1_UUID);
+        System.out.println(qoh);
+    }
+}
